5823052fa29e7e798db24e08b08ce0438efbffa8
Alan4747 <alan47m@gmail.com>
2018-08-11 17:06:46 +0300

    Added files.

---
+++ README.md
@@ -0,0 +1,12 @@
+## Instagram Automatic Account Creator Bot
+
+<p>This small piece of code will create an account automatically by randomizing names. You need Selenium library installed. This is an ongoing work. Beware that Instagram has security measures to prevent bot usage. Do not forget to change the Web driver you use from the part of the code:</p>
+
+```Python
+browser = webdriver.Chrome("your chrome driver path here")
+```
+Also, you can use other browsers with tweaking the code.
+```Python
+#Another browser
+browser = webdriver.Firefox("your firefox driver path here")
+```
---
+++ botAccountCreate.py
@@ -0,0 +1,37 @@
+﻿from selenium import webdriver
+from random import randint
+import time
+from selenium.webdriver.common.by import By
+import accountInfoGenerator as account
+
+browser= webdriver.Chrome("chrome web driver path here")
+browser.get("http://www.instagram.com")
+time.sleep(8) #time.sleep count can be changed depending on the Internet speed.
+name = account.username()
+
+#Fill the email value
+email_field = browser.find_element_by_name('emailOrPhone')
+email_field.send_keys(account.genEmail())
+print(account.genEmail())
+
+#Fill the fullname value
+fullname_field = browser.find_element_by_name('fullName')
+fullname_field.send_keys(account.generatingName())
+print(account.generatingName())
+#Fill username value
+username_field = browser.find_element_by_name('username')
+username_field.send_keys(name)
+print(name)
+#Fill password value
+password_field  = browser.find_element_by_name('password')
+password_field.send_keys('aa12345bb12345cc'+name) #You can determine another password here.
+
+submit = browser.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[6]/span/button')
+submit.click()
+time.sleep(8)
+
+print('Registering....')
+
+
+
+
