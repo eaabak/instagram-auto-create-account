53e4ca3be4514ae06bc0a569d704822543070802
alan4747 <alan47m@gmail.com>
2020-12-20 16:11:03 +0300

    xPaths changed.

--- botAccountCreate.py
+++ botAccountCreate.py
@@ -11,7 +11,7 @@ import fakeMail as email
 
 browser= webdriver.Chrome("your chrome driver path here")
 browser.get("https://www.instagram.com/accounts/emailsignup/")
-time.sleep(8) #time.sleep count can be changed depending on the Internet speed.
+time.sleep(8)
 name = account.username()
 
 #Fill the email value
@@ -20,33 +20,37 @@ fake_email = email.getFakeMail()
 email_field.send_keys(fake_email)
 print(fake_email)
 
-#Fill the fullname value
+# Fill the fullname value
 fullname_field = browser.find_element_by_name('fullName')
 fullname_field.send_keys(account.generatingName())
 print(account.generatingName())
-#Fill username value
+# Fill username value
 username_field = browser.find_element_by_name('username')
 username_field.send_keys(name)
 print(name)
-#Fill password value
-password_field  = browser.find_element_by_name('password')
-password_field.send_keys(account.generatePassword()) #You can determine another password here.
-WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/article/div/div[1]/div/form/div[7]/div/button"))).click()
+# Fill password value
+password_field = browser.find_element_by_name('password')
+password_field.send_keys(account.generatePassword())  # You can determine another password here.
+print(account.generatePassword())
+WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/form/div[7]/div/button"))).click()
 
 time.sleep(8)
 
 #Birthday verification
-browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/article/div/div[1]/div/div[4]/div/div/span/span[1]/select/option[3]").click()
-WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/article/div/div[1]/div/div[4]/div/div/span/span[2]/select"))).click()
-browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/article/div/div[1]/div/div[4]/div/div/span/span[2]/select/option[12]").click()
-WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/article/div/div[1]/div/div[4]/div/div/span/span[3]/select"))).click()
-browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/article/div/div[1]/div/div[4]/div/div/span/span[3]/select/option[26]").click()
-WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/article/div/div[1]/div/div[6]"))).click()
+browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[1]/select").click()
+WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[1]/select/option[4]"))).click()
 
+browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[2]/select").click()
+WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[2]/select/option[10]"))).click()
+
+browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[3]/select").click()
+WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[3]/select/option[27]"))).click()
+
+WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/div[6]/button"))).click()
+time.sleep(3)
+#
 fMail = fake_email[0].split("@")
 mailName = fMail[0]
-domain =fMail[1]
+domain = fMail[1]
 instCode = verifiCode.getInstVeriCode(mailName, domain, browser)
 browser.find_element_by_name('email_confirmation_code').send_keys(instCode, Keys.ENTER)
-
-
