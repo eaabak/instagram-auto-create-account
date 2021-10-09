168c5472ec6501eb457b0fc9986bc5c40543cb7c
pOKzy <87678996+pokzy@users.noreply.github.com>
2021-10-09 03:30:22 +0300

    fixed bugs & new features
    fixed the argument errors + wrong printed password.
    fixed the cookie error.
    added autowrite login + password into txt file.
    added autoaccept notifications + autologout from the account.
    added driver.quit after account logout.
    (there's chrome driver path, if you want firefox, change it yourself.)

--- botAccountCreate.py
+++ botAccountCreate.py
@@ -10,32 +10,23 @@ import fakeMail as email
 import time
 import argparse
 
-parser = argparse.ArgumentParser()
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("--firefox", action="store_true", help="Use Firefox - geckodriver")
-group.add_argument("--chrome", action="store_true", help = "Use Chrome - chromedriver")
-
 args = parser.parse_args()
 ua = UserAgent()
 userAgent = ua.random
 print(userAgent)
 
-if args.firefox:
-    profile = webdriver.FirefoxProfile()
-    profile.set_preference("general.useragent.ovrride", userAgent)    
-    driver = webdriver.Firefox(firefox_profile=profile, executable_path=r"your gecko driver here")
 
+#replace 'your path here' with your chrome binary absolute path
+driver = webdriver.Chrome(r'your path here')
 
-if args.chrome:
-    from selenium.webdriver.chrome.options import Options
-    options = Options()
-    options.add_argument(f'user-agent={userAgent}')
-    driver= webdriver.Chrome(options=options, executable_path=r"your chrome driver here")
+#saves the login & pass into accounts.txt file.
+acc = open("accounts.txt", "a")
 
 driver.get("https://www.instagram.com/accounts/emailsignup/")
 time.sleep(8)
 try:
-	cookie = driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/button[1]').click()
+    cookie = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH,
+                                                                         '/html/body/div[3]/div/div/button[1]'))).click()
 except:
 	pass
 name = account.username()
@@ -50,14 +41,21 @@ print(fake_email)
 fullname_field = driver.find_element_by_name('fullName')
 fullname_field.send_keys(account.generatingName())
 print(account.generatingName())
+
 # Fill username value
 username_field = driver.find_element_by_name('username')
 username_field.send_keys(name)
 print(name)
+
 # Fill password value
 password_field = driver.find_element_by_name('password')
-password_field.send_keys(account.generatePassword())  # You can determine another password here.
-print(account.generatePassword())
+acc_password = account.generatePassword()
+password_field.send_keys(acc_password) # You can determine another password here.
+
+print(name+":"+acc_password, file=acc)
+
+acc.close()
+
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/form/div[7]/div/button"))).click()
 
 time.sleep(8)
@@ -81,6 +79,17 @@ domain = fMail[1]
 instCode = verifiCode.getInstVeriCode(mailName, domain, driver)
 driver.find_element_by_name('email_confirmation_code').send_keys(instCode, Keys.ENTER)
 time.sleep(10)
+
+#accepting the notifications.
+driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[3]/button[2]").click()
+time.sleep(2)
+
+#logout
+driver.find_element_by_xpath(
+    "//*[@id='react-root']/section/nav/div[2]/div/div/div[3]/div/div[5]/span/img").click()
+driver.find_element_by_xpath(
+    "//*[@id='react-root']/section/nav/div[2]/div/div/div[3]/div/div[5]/div[2]/div[2]/div[2]/div[2]/div").click()
+
 try:
     not_valid = driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[4]/div')
     if(not_valid.text == 'That code isn\'t valid. You can request a new one.'):
@@ -94,3 +103,6 @@ try:
       confInput.send_keys(instCodeNew, Keys.ENTER)
 except:
       pass
+
+time.sleep(5)
+driver.quit()
